76399b214b7409ca8934d242966ab88aae462351
pupu <li18736761061@163.com>
2025-09-18 16:21:56 +0800

    修改路径

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
 export default defineConfig({
-    base: './',
+    base: '/tour-plan-new',
     plugins: [vue()],
     resolve: {
         alias: {
